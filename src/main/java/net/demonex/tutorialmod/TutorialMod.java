fd6cbaea0b7abbde59e79e60e7861037a5300c32
Nexeran <chris352910@yahoo.com>
2022-11-16 22:33:26 -0600

    added Zircon block,  Zircon ore, Deepslate Zircon Ore, Netherrack Zircon Ore, Endstone Zircon Ore

--- src/main/java/net/demonex/tutorialmod/TutorialMod.java
+++ src/main/java/net/demonex/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.demonex.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.demonex.tutorialmod.block.ModBlocks;
 import net.demonex.tutorialmod.item.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -22,6 +23,7 @@ public class TutorialMod {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
