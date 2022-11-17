5bc6576ea1ee3abe6af25e7e4826636cbfb40679
Nexeran <chris352910@yahoo.com>
2022-11-17 00:32:46 -0600

    renamed mod from tutorial mod to Zircon mod

---
+++ src/main/java/net/demonex/zirconmod/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.demonex.zirconmod.item;
+
+import net.demonex.zirconmod.ZirconMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ZirconMod.MOD_ID);
+    // Zircon
+    public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ZIRCON_TAB)));
+
+    // Raw Zircon
+    public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ZIRCON_TAB)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
